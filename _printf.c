50cd45a17bdf2d5a1425212a0d45c090c45137b2
Bra Koo$e <72326008+BraKoose@users.noreply.github.com>
2023-09-17 12:52:31 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,55 +1,54 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdarg.h> // allows functions to accept an indefinite number of arguments.
+#include <stdarg.h>
+
 /**
-  * _printf - prints objects
-  * @format: type
-  * Return: 0
+ * _printf - prints objects
+ * @format: type
+ * Return: 0
  */
 int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format); //enables access to the variable arguments
-    int count = 0; // To keep track of characters printed
-    while (*format) {
-        if (*format == '%') {
-            format++; // Move past '%'
-            switch (*format) { // executes statements after the first case clause with a matching value, until a break statement is encountered
-                case 'c':
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    {
-                        const char *str = va_arg(args, const char *);
-                        while (*str) {
-                            putchar(*str);
-                            str++;
-                            count++;
-                        }
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                case 'r':
-                 putchar('%'); // Print the '%' character
-                 putchar('r'); // Print the 'r' character
-                 count += 2;   // Increment 'count' by the number of characters printed
-                 break;
+	va_list args;
+	va_start(args, format);
+	int count = 0;
 
-
-                default: // Handle unsupported format specifier
-                    putchar('?');
-                    count++;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++; // Move to the next character in the format string
-    }
-    va_end(args); // performs cleanup for an ap object initialized by a call to va_start
-    return count;
+	while (*format) {
+		if (*format == '%') {
+			format++;
+			switch (*format) {
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's': {
+					const char *str = va_arg(args, const char *);
+					while (*str) {
+						putchar(*str);
+						str++;
+						count++;
+					}
+				}
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				case 'r':
+					putchar('%');
+					putchar('r');
+					count += 2;
+					break;
+				default:
+					putchar('?');
+					count++;
+					break;
+			}
+		} else {
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return count;
 }
