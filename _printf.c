80b3de1b4cfbeb0017396501c5bcc2661eb7aeb9
Bra Koo$e <72326008+BraKoose@users.noreply.github.com>
2023-09-17 14:59:44 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,55 +3,38 @@
 /**
  * _printf - Prints formatted output to stdout.
  * @format: A pointer to the format string.
- *
  * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-	int character_count = 0;
-	va_list args;
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-			case 'c':
-				putchar(va_arg(args, int));
-				character_count++;
-				break;
-			case 's':
-			{
-				char *str = va_arg(args, char *);
-				while (*str)
-				{
-					putchar(*str);
-					str++;
-					character_count++;
-				}
-			}
-				break;
-			case '%':
-				putchar('%');
-				character_count++;
-				break;
-			default:
-				putchar('?');
-				character_count++;
-				break;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			character_count++;
-		}
-		format++;
-	}
-
-	va_end(args);
-	return (character_count);
+    int character_count = 0;
+    va_list args;
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format)
+            {
+                char specifier = *format;
+                int (*print_function)(va_list) = get_print_function(specifier);
+                if (print_function)
+                    character_count += print_function(args);
+                else
+                {
+                    putchar('?'); // Handle unknown format specifier with '?'
+                    character_count++;
+                }
+            }
+        }
+        else
+        {
+            putchar(*format);
+            character_count++;
+        }
+        format++;
+    }
+    va_end(args);
+    return character_count;
 }
