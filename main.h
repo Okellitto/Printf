b0acf14af7e163c33d74701d4efdb3c283c6ee34
Bra Koo$e <72326008+BraKoose@users.noreply.github.com>
2023-09-17 14:55:54 +0000

    Update main.h

--- main.h
+++ main.h
@@ -3,6 +3,21 @@
 
 #include <stdarg.h>
 
+/**
+ * struct print_function - A structure for print functions.
+ * @specifier: The format specifier.
+ * @func: A pointer to the corresponding print function.
+ */
+typedef struct print_function
+{
+    char specifier;
+    int (*func)(va_list);
+} print_function_t;
+
 int _printf(const char *format, ...);
+
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
 
 #endif /* MAIN_H */
