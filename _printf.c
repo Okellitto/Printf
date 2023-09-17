52ceaeaab4bbf258ecec85165658d952053aa963
Bra Koo$e <72326008+BraKoose@users.noreply.github.com>
2023-09-17 14:48:37 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,50 +8,50 @@
  */
 int _printf(const char *format, ...)
 {
-    int character_count = 0;
-    va_list args;
-    va_start(args, format);
+	int character_count = 0;
+	va_list args;
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    character_count++;
-                    break;
-                case 's':
-                    {
-                        char *str = va_arg(args, char *);
-                        while (*str)
-                        {
-                            putchar(*str);
-                            str++;
-                            character_count++;
-                        }
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    character_count++;
-                    break;
-                default:
-                    putchar('?'); // Handle unknown format specifier with '?'
-                    character_count++;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            character_count++;
-        }
-        format++;
-    }
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				putchar(va_arg(args, int));
+				character_count++;
+				break;
+			case 's':
+			{
+				char *str = va_arg(args, char *);
+				while (*str)
+				{
+					putchar(*str);
+					str++;
+					character_count++;
+				}
+			}
+				break;
+			case '%':
+				putchar('%');
+				character_count++;
+				break;
+			default:
+				putchar('?');
+				character_count++;
+				break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			character_count++;
+		}
+		format++;
+	}
 
-    va_end(args);
-    return character_count;
+	va_end(args);
+	return (character_count);
 }
