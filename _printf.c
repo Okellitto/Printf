a84f77916311b0da4d607b79af07e71aff6eb9b2
Bra Koo$e <72326008+BraKoose@users.noreply.github.com>
2023-09-17 15:12:59 +0000

    Update _printf.c final

--- _printf.c
+++ _printf.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _printf - Prints formatted output to stdout.
  * @format: A pointer to the format string.
+ *
  * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
@@ -10,6 +12,7 @@ int _printf(const char *format, ...)
     int character_count = 0;
     va_list args;
     va_start(args, format);
+
     while (*format)
     {
         if (*format == '%')
@@ -35,6 +38,7 @@ int _printf(const char *format, ...)
         }
         format++;
     }
+
     va_end(args);
     return character_count;
 }
