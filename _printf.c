8dfa76ac963010265c78e09bdd5a98c412c8ad27
Okellitto <okellitto@gmail.com>
2023-09-19 15:17:46 -0700

    Im not going anywhere. You can print that wherever you want to. Im here and Im a Spur for life

--- _printf.c
+++ _printf.c
@@ -5,16 +5,19 @@
 
 int _printf(const char *format, ...) 
 {
-    va_list args;
-    va_start(args, format);
+	va_list args;
+
+	va_start(args, format);
     
-    int count = 0; // To keep track of characters printed
-    
-    while (*format)
-    {
-	if (*format == '%') {
-	format++; // Move past '%'
-	switch (*format) {
+	int count = 0;
+
+	while (*format)
+	{
+	if (*format == '%')
+	{
+	format++;
+	switch (*format)
+	{
 	case 'c':
 	putchar(va_arg(args, int));
 	count++;
@@ -22,7 +25,8 @@ int _printf(const char *format, ...)
 	case 's':
 	{
 	const char *str = va_arg(args, const char *);
-	while (*str) {
+	while (*str)
+	{
 	putchar(*str);
 	str++;
 	count++;
@@ -43,7 +47,7 @@ int _printf(const char *format, ...)
 	putchar(*format);
 	count++;
         }
-	format++; // Move to the next character in the format string
+	format++;
     }
 	va_end(args);
 	return count;
