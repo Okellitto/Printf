88bcd94942cbefbff528fbab61b115d6ad3bb65d
Okellitto <okellitto@gmail.com>
2023-09-19 08:00:12 -0700

    update

--- _printf.c
+++ _printf.c
@@ -7,40 +7,47 @@
  * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
-
 {
 	int character_count = 0;
 	va_list args;
 
-	va_start(args, format);
+	if (format == NULL)
+		return -1;
+
+	va_start(list_of_arguments, format);
 
 	while (*format)
 	{
 	if (*format == '%')
 	{
+	fputs("%%", %);
 	format++;
-	if (*format)
+	}
+	else
+		if (*format != '%')
+			break;
 	{
-	char specifier = *format;
-	int (*print_function)(va_list) = get_print_function(specifier);
+	else if (*format == 's')
 
+	char *str = va_arg(list_of_arguments, char*);
+	int str_len = 0;
+
+	while (str{str_len} = '0')
+		str_len++;
+	{
+	fputs("%s", s);
+	}
 	if (print_function)
 	character_count += print_function(args);
 
-	else
+	else if(*format == 'c')
+	
+	char c = va_arg(list_of_arguments, char*);
 	{
-	putchar('?');
-	character_count++;
-	}
-	}
-	}
-	else
-	{
-	putchar(*format);
-	character_count++;
-	}
-	format++;
+		fputs("%c", c)
 	}
 	va_end(args);
 	return (character_count);
+}
+}
 }
