577e806a5fe9020546d1f47bc9c4f3d589875992
Okellitto <okellitto@gmail.com>
2023-09-19 14:26:19 -0700

    Im not going anywhere. You can print that wherever you want to. Im here and Im a Spur for life

--- _printf.c
+++ _printf.c
@@ -22,37 +22,37 @@ int _printf(const char *format, ...)
 	{
 	if (*format == '%')
 	{
-	fputs("%%", %);
+	fputs(%%, %);
 	character_count++;
 	}
 	else
-
+	{
 	if (*format == '\0')
 	break;
-
+	}
+	{
 	if (*format != '%')
 	character_count++;
-	{
-	else if (*format == 'c')
-		char c = va_arg(list_of_arguments, char*);
+	}
+	if (*format == 'c')
+	char c = va_arg(list_of_arguments, char*);
         {
-                fputs("%c", c);
+	fputs(%c, c);
 	}
 	else if (*format == 's')
 
 	char *str = va_arg(list_of_arg, char*);
 	int str_len = 0;
 
-	while (str(str_len) = '0')
+	while (str(str_len) == '0')
 		str_len++;
 	{
-	fputs("%s", s);
+	fputs(%s, s);
 	}
 	if (print_function)
 	character_count += print_function(args);
 
 	va_end(args);
 	return (character_count);
-	}
 	}
 }
