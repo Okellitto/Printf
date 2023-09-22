b08cf2cad01ddf935dc31c2d6b6db66ce2ba481c
Okellitto <okellitto@gmail.com>
2023-09-22 13:59:10 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -24,55 +24,8 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-<<<<<<< HEAD
-	int count = 0;
-
-	while (*format)
-	{
-	if (*format == '%')
-	{
-	format++;
-	switch (*format)
-	{
-	case 'c':
-	putchar(va_arg(args, int));
-	count++;
-	break;
-
-	case 's':
-	{
-	const char *str = va_arg(args, const char *);
-
-	while (*str)
-	{
-	putchar(*str);
-	str++;
-	count++;
-	}
-	}
-	break;
-	case '%':
-	putchar('%');
-	count++;
-	break;
-	default:
-	putchar('?');
-	count++;
-	break;
-	}
-	
-	else
-	{
-		putchar(*format);
-		count++;
-		format++;
-	}
-		va_end(args);
-	return (count);
-=======
 	printed_characters = parser(format, function_list, args);
 	va_end(args);
 
 	return (printed_characters);
->>>>>>> cbef9b578ec6d0a5fb405a3f34f8ed0221af398d
 }
